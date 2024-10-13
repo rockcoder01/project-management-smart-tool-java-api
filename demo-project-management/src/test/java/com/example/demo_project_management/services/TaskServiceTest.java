5b07f3c805c26104d46058b4324fd2abc439a29c
rockcoder01 <ahirmmm6@gmail.com>
2024-10-13 18:43:29 +0530

    h2 db use on build and run mockito injection

--- demo-project-management/src/test/java/com/example/demo_project_management/services/TaskServiceTest.java
+++ demo-project-management/src/test/java/com/example/demo_project_management/services/TaskServiceTest.java
@@ -13,9 +13,11 @@ import com.example.demo_project_management.repository.TaskRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -32,8 +34,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest
-//@Disabled("Test class disabled for refactoring")
+@ExtendWith(MockitoExtension.class)
 public class TaskServiceTest {
 
     @Mock
